d6560f0b88a8c31dd3c8093257368d1b64d59b38
DavidZyy <1929772352@qq.com>
2024-12-28 14:21:52 +0800

    [OPT] refine the codes of ModuleList

--- tensorLib/include/nn/container.hpp
+++ tensorLib/include/nn/container.hpp
@@ -7,36 +7,13 @@ namespace nn {
 template <typename dtype> class ModuleList : public Module<dtype> {
 public:
   // Default constructor
-  ModuleList() = default;
-  ModuleList(std::string device_type);
+    ModuleList() = default;
+    ModuleList(std::string device_type);
 
-  // Append a new module to the list
-  void append(std::shared_ptr<Module<dtype>> module) {
-    modules_.push_back(module);
-  }
-
-  // Access a module by index
-  std::shared_ptr<Module<dtype>> operator[](int index) const {
-    if (index < 0 || index >= modules_.size()) {
-      throw std::out_of_range("ModuleList index out of range.");
-    }
-    return modules_[index];
-  }
-
-  // Get the size of the ModuleList
-  int size() const { return modules_.size(); }
-
-  // Forward pass through all modules in the list
-  Tensor<dtype> forward(const Tensor<dtype> &input) const {
-    Tensor<dtype> current_input = input;
-
-    // Sequentially pass through all modules in the list
-    for (const auto &module : modules_) {
-      current_input = module->forward(current_input);
-    }
-
-    return current_input;
-  }
+    void append(std::shared_ptr<Module<dtype>> module);
+    std::shared_ptr<Module<dtype>> operator[](int index) const;
+    int size() const;
+    Tensor<dtype> forward(const Tensor<dtype> &input) const;
 
 private:
   // Vector to hold the list of shared pointers to modules
--- tensorLib/src/nn/container.cpp
+++ tensorLib/src/nn/container.cpp
@@ -8,4 +8,37 @@ template class ModuleList<int>;
 template <typename dtype>
 ModuleList<dtype>::ModuleList(std::string device_type) : Module<dtype>(device_type) {}
 
+// Append a new module to the list
+template <typename dtype>
+void ModuleList<dtype>::append(std::shared_ptr<Module<dtype>> module) {
+    modules_.push_back(module);
+}
+
+// Access a module by index
+template <typename dtype>
+std::shared_ptr<Module<dtype>> ModuleList<dtype>::operator[](int index) const {
+    if (index < 0 || index >= modules_.size()) {
+        throw std::out_of_range("ModuleList index out of range.");
+    }
+    return modules_[index];
+}
+
+// Get the size of the ModuleList
+template <typename dtype>
+int ModuleList<dtype>::size() const { return modules_.size(); }
+
+// Forward pass through all modules in the list
+template <typename dtype>
+Tensor<dtype> ModuleList<dtype>::forward(const Tensor<dtype> &input) const {
+    Tensor<dtype> current_input = input;
+
+    // Sequentially pass through all modules in the list
+    for (const auto &module : modules_) {
+        current_input = module->forward(current_input);
+    }
+
+    return current_input;
+}
+
+
 } // namespace nn
